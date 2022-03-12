ac7afd69d334a34117e4c81e1b2a8cb65e6449c7
Meharpreet Singh Nanda <msnanda515@gmail.com>
2022-03-12 16:49:51 -0700

    Add header

--- App.js
+++ App.js
@@ -2,10 +2,12 @@ import { StyleSheet, Text, View } from 'react-native';
 import CarModel from "./components/CarModelView/CarModel";
 import Constants from "./assets/constants";
 import CarsList from "./components/CarsList/CarsList";
+import Header from "./components/Header/Header";
 
 export default function App() {
   return (
     <View style={styles.container}>
+      <Header />
       <CarsList />
     </View>
   );
